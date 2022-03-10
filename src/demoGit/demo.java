d556db2ab470784761e39bff8bc9733e12ceda00
eppys <eppys@LAPTOP-Q08K87KM>
2022-03-10 16:17:56 +0530

    USER2- second commit

--- src/demoGit/demo.java
+++ src/demoGit/demo.java
@@ -5,6 +5,12 @@ import org.testng.annotations.Test;
 public class demo {
 @Test
 public void demog() {
-	System.out.println("SDET-30 First Commit");
+	System.out.println("user1 First Commit");
+	
+	
+	
+	
+	
+	System.out.println("user 2 first commit");
 }
 }
